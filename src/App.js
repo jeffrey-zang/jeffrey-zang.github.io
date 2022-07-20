46d1e57942dbef7bd229b878bc180cb4d8cf5eee
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-20 12:56:34 -0400

    Animations using AOS library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import AOS from 'aos'
+AOS.init();
 
 const App = () => {
   
   return (
     <>
 
-    <div style = {{height: '100%'}}>
+    <div className = 'container'>
       <div className = 'left' >
         
         <i className="bi bi-code" style = {{color: '#E7F6F2', fontSize: '27px'}}></i>
@@ -34,21 +36,34 @@ const App = () => {
 
       </div>
 
-      <div className = 'right' style = {{marginTop: '15vh'}}>
+      <div className = 'right'>
 
         <h1 style = {{color: '#a5ff90', fontWeight: '900', fontSize: '60px', wordWrap: 'normal'}}>Portfolio</h1>
         <h1 style = {{lineHeight: '40px', marginTop: '20px', fontSize: '30px'}}>
           > My projects, creations, and experiences
         </h1>
         <button className = 'btn'>
-          View Portfolio
+          <i className="bi bi-folder2-open"></i>
+          {' View Portfolio'}
         </button>
         
         <h1 style = {{color: '#a5ff90', fontWeight: '900', fontSize: '60px', wordWrap: 'normal'}}>Resume</h1>
         <h1 style = {{lineHeight: '40px', marginTop: '20px', fontSize: '30px'}}>
-          > My achievements, extracurriculars, work and volunteering experience</h1>
+          > My achievements, extracurriculars, work and volunteering experience
+        </h1>
+        <button className = 'btn' onClick = {
+          () => {
+            window.open('https://docs.google.com/document/d/1b6urcHHJYErsZNCe6qV3wdkqDbrzLin93wucKvwnnzc/view')
+          }
+        }>
+          <i className="bi bi-briefcase"></i>
+          {' View Resume'}
+        </button>
+
       </div>
     </div>
+
+    <h1 style = {{color: '#a5ff90', fontWeight: '900', fontSize: '80px', textAlign:'center', marginTop: ''}}>Portfolio</h1>
 
     </>
   )
