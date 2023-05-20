1a0eb53dda81fd518891a980a33b32eb8ab7a0fc
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-05-19 20:01:18 -0400

    init: initialize vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  components: {
+    Landing
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    among
+    <Landing/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>amongus</h1>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
+
